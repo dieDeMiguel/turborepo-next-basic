07920dc2813ad47934fe674274615b80ded7c997
Progress with the component <deMiguel@newsaktuell.de>
2024-12-13 07:28:33 +0100

    Update Tailwind CSS configuration to include app and components directories; modify TypeScript config to set root directory

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Include all files in the pages directory
+    "./components/**/*.{js,ts,jsx,tsx}", // Include all files in the components directory
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
