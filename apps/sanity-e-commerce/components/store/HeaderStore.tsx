75e891ab192e2c186d8569b70f9b75619dd518e8
Progress with the component <55700336+dieDeMiguel@users.noreply.github.com>
2024-12-14 17:48:46 +0100

    refactor: clean up HeaderStore component, improve code consistency, and update dependencies

--- apps/sanity-e-commerce/components/store/HeaderStore.tsx
+++ apps/sanity-e-commerce/components/store/HeaderStore.tsx
@@ -1,27 +1,25 @@
-'use client'
+'use client';
 
-import { ClerkLoaded, SignInButton, UserButton, useUser } from '@clerk/nextjs'
-import Link from 'next/link'
-import Form from 'next/form'
-import { PackageIcon, TrolleyIcon } from '@sanity/icons'
-import { SignedIn } from '@clerk/clerk-react'
-import useBasketStore from '@/app/store/store'
+import { ClerkLoaded, SignInButton, UserButton, useUser } from '@clerk/nextjs';
+import Link from 'next/link';
+import Form from 'next/form';
+import { PackageIcon, TrolleyIcon } from '@sanity/icons';
+import { SignedIn } from '@clerk/clerk-react';
+import useBasketStore from '@/app/store/store';
 
 function HeaderStore() {
-  const { user } = useUser()
-  const itemCount = useBasketStore((state) =>
-    state.items.reduce((total, item) => total + item.quantity, 0)
-  )
+  const { user } = useUser();
+  const itemCount = useBasketStore((state) => state.items.reduce((total, item) => total + item.quantity, 0));
 
   /**
    * Cool way to secure login with a passkey
    */
   const createClerkPasskey = async () => {
     try {
-      const response = await user?.createPasskey()
-      console.log('Response: ', JSON.stringify(response, null, 2))
+      const response = await user?.createPasskey();
+      console.log('Response: ', JSON.stringify(response, null, 2));
     } catch (error) {
-      console.error('Error: ', JSON.stringify(error, null, 2))
+      console.error('Error: ', JSON.stringify(error, null, 2));
     }
 
     const response = await fetch('/api/clerk/create-passkey', {
@@ -30,42 +28,39 @@ function HeaderStore() {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({}),
-    })
+    });
 
-    console.log(response)
-  }
+    console.log(response);
+  };
 
   return (
-    <header className="flex flex-wrap justify-between items-center px-4 py-2">
+    <header className="flex flex-wrap items-center justify-between px-4 py-2">
       {/* Top Row  */}
-      <div className="flex w-full flex-wrap justify-center items-center">
+      <div className="flex w-full flex-wrap items-center justify-center">
         <Link
           href="/store"
-          className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
+          className="mx-auto cursor-pointer text-2xl font-bold text-blue-500 hover:opacity-50 sm:mx-0"
         >
           Shopr
         </Link>
 
-        <Form
-          action="/store/search"
-          className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
-        >
+        <Form action="/store/search" className="mt-2 w-full sm:mx-4 sm:mt-0 sm:w-auto sm:flex-1">
           <input
-            className="bg-gray-100 text-gray-800 px-4 py-2 rounded focus:otuline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-30 border w-full max-w-4xl"
+            className="focus:otuline-none w-full max-w-4xl rounded border bg-gray-100 px-4 py-2 text-gray-800 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-30"
             type="text"
             name="query"
             placeholder="Search for products"
           />
         </Form>
 
-        <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 sm:flex-none">
+        <div className="mt-4 flex flex-1 items-center space-x-4 sm:mt-0 sm:flex-none">
           <Link
-            className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
+            className="relative flex flex-1 items-center justify-center space-x-2 rounded bg-blue-500 px-4 py-2 text-sm font-bold text-white hover:bg-blue-700 sm:flex-none sm:justify-start"
             href="/store/basket"
           >
-            <TrolleyIcon className="w-6 h-6" />
+            <TrolleyIcon className="h-6 w-6" />
             {/* Span item count // Global  */}
-            <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full size-5 flex items-center justify-center text-xs">
+            <span className="absolute -right-2 -top-2 flex size-5 items-center justify-center rounded-full bg-red-500 text-xs text-white">
               {itemCount}
             </span>
             <span className="hidden sm:block">My Basket</span>
@@ -75,10 +70,10 @@ function HeaderStore() {
           <ClerkLoaded>
             <SignedIn>
               <Link
-                className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
+                className="relative flex flex-1 items-center justify-center space-x-2 rounded bg-blue-500 px-4 py-2 text-sm font-bold text-white hover:bg-blue-700 sm:flex-none sm:justify-start"
                 href="/store/orders"
               >
-                <PackageIcon className="w-6 h-6" />
+                <PackageIcon className="h-6 w-6" />
                 {/* Span item count // Global  */}
                 <span className="hidden sm:block">My Orders</span>
               </Link>
@@ -88,7 +83,7 @@ function HeaderStore() {
               <div className="flex items-center space-x-2">
                 <UserButton />
 
-                <div className="hidden sm:block text-xs">
+                <div className="hidden text-xs sm:block">
                   <p className="text-gray-400">Welcome Back</p>
                   <p className="font-bold">{user.fullName}</p>
                 </div>
@@ -99,7 +94,7 @@ function HeaderStore() {
 
             {user?.passkeys.length === 0 && (
               <button
-                className="bg-white hover:bg-blue-700 hover:text-white motion-safe:animate-pulse text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border text-xs text-nowrap"
+                className="text-nowrap rounded border border-blue-300 bg-white px-4 py-2 text-xs font-bold text-blue-500 hover:bg-blue-700 hover:text-white motion-safe:animate-pulse"
                 onClick={createClerkPasskey}
               >
                 Create passkey
@@ -109,6 +104,6 @@ function HeaderStore() {
         </div>
       </div>
     </header>
-  )
+  );
 }
-export default HeaderStore
+export default HeaderStore;
