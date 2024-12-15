588e33e6648bf40183d76259a9e30555df65638e
Progress with the component <55700336+dieDeMiguel@users.noreply.github.com>
2024-12-15 06:55:13 +0100

    changed to category selector's old approach

--- apps/sanity-e-commerce/components/ui/category-selector.tsx
+++ apps/sanity-e-commerce/components/ui/category-selector.tsx
@@ -2,22 +2,28 @@
 
 import { ChevronsUpDown, Check } from 'lucide-react';
 
-import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
-import { Command, CommandEmpty, CommandGroup, CommandItem, CommandList, CommandInput } from '@/components/ui/command';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 import { Category } from '@/sanity.types';
+import { useParams, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { Popover, PopoverContent, PopoverTrigger } from './popover';
+import { Button } from './button';
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from './command';
+import { cn } from '@/lib/utils';
 
-interface CategorySelectorProps {
-  categories: Category[];
-}
-
-export function CategorySelectorComponent({ categories }: CategorySelectorProps) {
+export default function CategorySelectorComponent({ categories }: { categories: Category[] }) {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState<string>('');
   const router = useRouter();
+  const navigation = useParams();
+
+  useEffect(() => {
+    if (navigation.slug && categories && categories.find((c) => c.slug?.current === navigation.slug)) {
+      const selectedCategory = categories.find((c) => c.slug?.current === navigation.slug);
+      if (selectedCategory) {
+        setValue(selectedCategory._id);
+      }
+    }
+  }, [navigation.slug, categories]);
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -26,26 +32,25 @@ export function CategorySelectorComponent({ categories }: CategorySelectorProps)
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="relative flex w-full max-w-full items-center justify-center space-x-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 hover:text-white sm:flex-none sm:justify-start"
+          className="relative mb-4 flex w-full max-w-full items-center justify-center space-x-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 hover:text-white sm:flex-none sm:justify-start"
         >
           {value ? categories.find((category) => category._id === value)?.title : 'Filter by Category'}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent>
+      <PopoverContent className="w-full p-0">
         <Command>
           <CommandInput
             placeholder="Search category..."
             className="h-9"
-            onKeyDown={(e: { key: string; currentTarget: { value: string } }) => {
+            onKeyDown={(e) => {
               if (e.key === 'Enter') {
                 const selectedCategory = categories.find((c) =>
                   c.title?.toLowerCase().includes(e.currentTarget.value.toLowerCase())
                 );
-
                 if (selectedCategory?.slug?.current) {
                   setValue(selectedCategory._id);
-                  router.push(`/categories/${selectedCategory.slug.current}`);
+                  router.push(`/store/categories/${selectedCategory.slug.current}`);
                   setOpen(false);
                 }
               }
@@ -65,7 +70,7 @@ export function CategorySelectorComponent({ categories }: CategorySelectorProps)
                   }}
                 >
                   {category.title}
-                  <Check className={cn('ml-auto h-4 w-4', value === category._id ? 'opacity-100' : 'opacity-0')} />
+                  <Check className={cn('ml-auto size-4', value === category._id ? 'opacity-100' : 'opacity-0')} />
                 </CommandItem>
               ))}
             </CommandGroup>
