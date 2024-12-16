f694ad6931e1e73b67b0393b9e2da7373fd9376c
Progress with the component <55700336+dieDeMiguel@users.noreply.github.com>
2024-12-16 19:08:59 +0100

    rollback changes

--- apps/sanity-e-commerce/app/(normal)/page.tsx
+++ apps/sanity-e-commerce/app/(normal)/page.tsx
@@ -1,34 +1,34 @@
 import React, { Suspense } from 'react';
 import BlackFridayBanner from '@/components/store/BlackFridayBanner';
-// import CountryInfo from '@/components/store/CountryInfo';
+import CountryInfo from '@/components/store/CountryInfo';
 import ProductsView from '@/components/store/ProductsView';
-// import { showCountry } from '@/experiments/flags';
+import { showCountry } from '@/experiments/flags';
 import { getAllCategories } from '@/sanity/lib/products/getAllCategories';
 import { getAllProducts } from '@/sanity/lib/products/getAllProducts';
-// import { FlagValues } from '@vercel/flags/react';
+import { FlagValues } from '@vercel/flags/react';
 import ProductGridSkeleton from '@/components/ui/skeleton';
 
 export const revalidate = 1800; // ISR: revalidate every 30 minutes
 
-// const LoadingCountryInfo = () => <ProductGridSkeleton />;
+const LoadingCountryInfo = () => <ProductGridSkeleton />;
 const LoadingProducts = () => <ProductGridSkeleton />;
 
 export default async function Page() {
-  const [products, categories] = await Promise.all([
+  const [products, categories, shouldShowCountry] = await Promise.all([
     getAllProducts(),
     getAllCategories(),
-    // showCountry(),
+    showCountry(),
   ]);
 
   return (
     <div className="mb-4 mt-4 flex flex-col gap-8 rounded-lg bg-gray-100 py-8">
       <BlackFridayBanner />
       <div className="container mx-auto flex flex-col items-center justify-center px-4">
-        {/* <Suspense fallback={<LoadingCountryInfo />}>{shouldShowCountry && <CountryInfo />}</Suspense> */}
+        <Suspense fallback={<LoadingCountryInfo />}>{shouldShowCountry && <CountryInfo />}</Suspense>
         <Suspense fallback={<LoadingProducts />}>
           <ProductsView products={products} categories={categories} />
         </Suspense>
-        {/* <FlagValues values={{ 'show-country': shouldShowCountry }} /> */}
+        <FlagValues values={{ 'show-country': shouldShowCountry }} />
       </div>
     </div>
   );
