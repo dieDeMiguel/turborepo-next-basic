b7ae130b6cd64934f8c47ea2b8e290dd176dc09b
Progress with the component <55700336+dieDeMiguel@users.noreply.github.com>
2024-12-17 13:29:16 +0100

    Refactor InputField component to use forwardRef and extend input props

--- packages/ui/src/input-field.tsx
+++ packages/ui/src/input-field.tsx
@@ -1,20 +1,25 @@
-import React from "react";
+import * as React from "react";
 
-interface InputFieldProps {
-  type: string;
+interface InputFieldProps extends React.ComponentProps<"input"> {
   name: string;
-  placeholder: string;
+  placeholder?: string;
 }
 
-const InputField: React.FC<InputFieldProps> = ({ type, name, placeholder }) => {
-  return (
-    <input
-      type={type}
-      name={name}
-      placeholder={placeholder}
-      className="w-full max-w-4xl rounded border bg-gray-100 px-4 py-2 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-    />
-  );
-};
+const InputField = React.forwardRef<HTMLInputElement, InputFieldProps>(
+  ({ className, type = "text", name, placeholder, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        className={`w-full max-w-4xl rounded-md border bg-gray-100 px-4 py-2 text-gray-800 shadow-sm placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+InputField.displayName = "InputField";
 
 export default InputField;
