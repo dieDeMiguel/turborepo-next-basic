406e5c24df4f7cafab8e35a0a6daef6a2584b9f8
Diego de M. <55700336+dieDeMiguel@users.noreply.github.com>
2025-08-07 10:43:14 +0200

    feat: add pool image feature and update flags for conditional rendering in the e-commerce app

--- apps/sanity-e-commerce/app/(normal)/page.tsx
+++ apps/sanity-e-commerce/app/(normal)/page.tsx
@@ -2,11 +2,12 @@ import React, { Suspense } from 'react';
 import BlackFridayBanner from '@/components/store/BlackFridayBanner';
 import CountryInfo from '@/components/store/CountryInfo';
 import ProductsView from '@/components/store/ProductsView';
-import { showAorB, showCountry } from '@/experiments/flags';
+import { showAorB, showCountry, showPoolImage } from '@/experiments/flags';
 import { getAllCategories } from '@/sanity/lib/products/getAllCategories';
 import { getAllProducts } from '@/sanity/lib/products/getAllProducts';
 import { FlagValues } from '@vercel/flags/react';
 import ProductGridSkeleton from '@/components/ui/skeleton';
+import Image from 'next/image';
 
 export const revalidate = 1800; // ISR: revalidate every 30 minutes
 
@@ -14,16 +15,18 @@ const LoadingCountryInfo = () => <ProductGridSkeleton />;
 const LoadingProducts = () => <ProductGridSkeleton />;
 
 export default async function Page() {
-  const [products, categories, shouldShowCountry, shouldShowAorB] = await Promise.all([
+  const [products, categories, shouldShowCountry, shouldShowAorB, shouldShowPoolImage] = await Promise.all([
     getAllProducts(),
     getAllCategories(),
     showCountry(),
     showAorB(),
+    showPoolImage(),
   ]);
 
   const flagValues = {
     'show-country': shouldShowCountry,
     'show-a-or-b': shouldShowAorB,
+    'show-pool-image': shouldShowPoolImage,
   };
 
   return (
@@ -34,7 +37,8 @@ export default async function Page() {
         <Suspense fallback={<LoadingProducts />}>
           <ProductsView products={products} categories={categories} />
         </Suspense>
-        <FlagValues values={{ 'show-country': shouldShowCountry }} />
+        {shouldShowPoolImage && <Image width={1000} height={1000} src="/pool.jpg" alt="Pool" className="mt-4" />}
+        <FlagValues values={{ 'show-country': shouldShowCountry, 'show-pool-image': shouldShowPoolImage }} />
       </div>
       <script
         // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
--- apps/sanity-e-commerce/experiments/flags.ts
+++ apps/sanity-e-commerce/experiments/flags.ts
@@ -9,3 +9,13 @@ export const showAorB = flag({
   key: 'show-a-or-b',
   decide: () => Math.random() > 0.5, // 50% chance to show
 });
+
+export const showSummerBanner = flag({
+  key: 'show-summer-banner',
+  decide: () => process.env.SHOW_SUMMER_BANNER === 'true',
+});
+
+export const showPoolImage = flag({
+  key: 'show-pool-image',
+  decide: () => process.env.SHOW_POOL_IMAGE === 'true',
+});
